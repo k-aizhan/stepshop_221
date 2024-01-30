f3db2bdc894bc493d91eed2e626a78fb1a7ed2ac
Aizhan <a_k_tut@mail.ru>
2024-01-30 21:49:49 +0600

    new

---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import index, products
+
+app_name = "main"
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('products/', products, name="products"),
+]
---
+++ mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    title = 'Главная страница'
+
+    context = {
+        'title':title,
+    }
+    return render(request, 'index.html', context)
+
+
+def products(request):
+    return render(request, 'products.html')
