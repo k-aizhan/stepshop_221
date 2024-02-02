d2e13690ad6e5b1de6679170859f3ea80ca07733
Aizhan <a_k_tut@mail.ru>
2024-02-02 21:42:39 +0600

    Урок 5

--- stepshop/urls.py
+++ stepshop/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from stepshop.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('mainapp.urls', namespace='main')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.NEDIA_ROOT)
+
